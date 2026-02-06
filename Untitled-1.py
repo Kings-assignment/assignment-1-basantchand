bf41d3b6c7c95b35e0325ba28ea4e7af48666f6b
basantchand <basantchand302@gmail.com>
2026-02-06 18:40:15 +0545

    final submission

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+name = "Student"
+course = "Introduction to Programming"
+print ("Welcome to Python Programming!")
+print ("Hello,", name)
+print ("Course:", course)
+print ("Python is easy to learn.")
+print ("Let's start coding!")
